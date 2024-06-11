c8f7295b6c229b28f0c4f2f610db07be7bb26187
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-11 11:38:20 +0900

    標準入力を開いて、入力値を取得できるようにした #4

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("hello!");
+    let mut input = String::new();
+
+    println!("何か入力してね:");
+
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("入力値が読み取れませんでした。");
+
+    println!("あなたが入力したのはこれだ！-> {}", input.trim());
 }
