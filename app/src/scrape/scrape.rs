461005450f373d6ea3f9bcac4bf12c2f64bc3385
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-11 15:37:24 +0900

    scraping を行うテストにエラーハンドリングを実装 #3

--- app/src/scrape/scrape.rs
+++ app/src/scrape/scrape.rs
@@ -1,4 +1,5 @@
 use scraper::{Html, Selector};
+use anyhow::{anyhow, Context,Result};
 use super::request::get_entire_html_tag_text;
 
 #[cfg(test)]
@@ -6,15 +7,20 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn sample_test() {
-        let html = get_entire_html_tag_text("https://example.com/").await.expect("スクレイピング対象サイトのアクセスに失敗しました。");
-        
+    async fn sample_test() -> Result<()> {
+        let html = 
+            get_entire_html_tag_text("https://example.com/")
+            .await.context("スクレイピング対象サイトのアクセスに失敗しました。")?;
+
         let fragment = Html::parse_fragment(&html);
-        let selector = Selector::parse("h1").unwrap();
-        
+        let selector = 
+            Selector::parse("h1")
+            .map_err(|e|  {anyhow!("CSS セレクタのパースに失敗しました: {}", e)})?;
+
         for element in fragment.select(&selector) {
             // example.com の h1 タグの中身は "Example Domain" なので、これを検証する
             assert_eq!(element.inner_html(), "Example Domain");
         }
+        Ok(())
     }
 }
