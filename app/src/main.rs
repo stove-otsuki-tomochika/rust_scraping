6144c6facc244e898c543599c01c70390fb677f0
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-11 12:36:17 +0900

    loop で標準入力が開きっぱなしになるようにした #4

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,20 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    let mut input = String::new();
+    loop {
+        let mut input = String::new();
 
-    println!("何か入力してね:");
+        println!("何か入力してね。終了するときは「exit」って入れてね:");
 
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("入力値が読み取れませんでした。");
+        io::stdin()
+            .lock()
+            .read_line(&mut input)
+            .expect("入力値が読み取れませんでした。");
 
-    println!("あなたが入力したのはこれだ！-> {}", input.trim());
+        if input.trim() == "exit" {
+            println!("終了します。");
+            break;
+        }
+        println!("あなたが入力したのはこれだ！-> {}", input.trim());
+    }
 }
