2316c9acdab1079df49db3f2d3168e28339655fe
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-11 13:46:39 +0900

    アサーションを書いてテストを完成させた #3

--- app/src/external_crate_test/test_reqwest.rs
+++ app/src/external_crate_test/test_reqwest.rs
@@ -9,7 +9,11 @@ mod tests {
         let url = "https://example.com/";
         let rc = reqwest::get(url).await.context("スクレイピング対象サイトの html タグ(全体)を取得できませんでした。")?;
         let contents = rc.text().await.context("html タグ -> String の変換に失敗しました")?;
-        println!("{:#?}", contents);
+
+        // 開始タグと終了タグが取得できているか確認することで、
+        // 疑似的に全体が取得出来たことを確認する
+        assert!(contents.contains("<html>"),"html 開始タグが取得できませんでした。");
+        assert!(contents.contains("</html>"),"html 閉じタグが取得できませんでした。");
         Ok(())
     }
 }
