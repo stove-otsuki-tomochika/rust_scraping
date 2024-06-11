79ea87e95ef3842c43a8bcbe3af97f8952856dbe
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-11 13:30:46 +0900

    テスト内のエラーメッセージを分かりやすくした #3

--- app/src/external_crate_test/test_reqwest.rs
+++ app/src/external_crate_test/test_reqwest.rs
@@ -2,12 +2,14 @@
 mod tests {
     use reqwest;
     use tokio;
+    use anyhow::{Context, Result};
 
     #[tokio::test]
-    async fn test_fetch_entire_html_tag() {
+    async fn test_fetch_entire_html_tag() -> Result<()> {
         let url = "https://example.com/";
-        let rc = reqwest::get(url).await.unwrap();
-        let contents = rc.text().await.unwrap();
+        let rc = reqwest::get(url).await.context("スクレイピング対象サイトの html タグ(全体)を取得できませんでした。")?;
+        let contents = rc.text().await.context("html タグ -> String の変換に失敗しました")?;
         println!("{:#?}", contents);
+        Ok(())
     }
 }
