b4ade6b344e79b3e56c7269c716338519b8a929f
stove-otsuki-tomochika <pc-017@st-ove.co.jp>
2024-06-12 10:57:59 +0900

    標準入力の mock を作っている部分を分かりやすく命名した #3

--- app/src/cli/mod.rs
+++ app/src/cli/mod.rs
@@ -12,12 +12,19 @@ mod tests {
 
     use super::*;
 
+    // 標準入力する値を受け取り、
+    // その入力値が入力された状態の mock を生成する
+    fn stdin_mock_with_inputted_text(input: &str) -> Cursor<&str> {
+        Cursor::new(input)
+    }
+
     // open_stdin を使うと、標準入力が開ける
     #[test]
     fn test_open_stdin() {
-        let cursor = Cursor::new("テスト入力");
-        let mut input = String::new();
-        open_stdin(cursor, &mut input);
-        assert_eq!(input, "テスト入力");
+        let stdin_mock = stdin_mock_with_inputted_text("テスト入力");
+        let mut input_from_user = String::new();
+
+        open_stdin(stdin_mock, &mut input_from_user);
+        assert_eq!(input_from_user, "テスト入力");
     }
 }
